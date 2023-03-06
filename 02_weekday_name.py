707d4ce6b230d1f1cd7e4494f73ee7f4960a86a7
Huse Kivrak <huse7@pm.me>
2023-03-06 18:57:35 -0500

    questions 1-5

--- 02_weekday_name.py
+++ 02_weekday_name.py
@@ -1,14 +1,22 @@
 def weekday_name(day_of_week):
     """Return name of weekday.
-    
+
         >>> weekday_name(1)
         'Sunday'
-        
+
         >>> weekday_name(7)
         'Saturday'
-        
+
     For days not between 1 and 7, return None
-    
+
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    days = [None, 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'] # 8
+    if (day_of_week > len(days)):
+        return None
+    elif day_of_week < 1:
+        return None
+    else:
+        return days[day_of_week]
+
